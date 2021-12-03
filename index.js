a0b05780a2e46dc5d9199af013eb6aa3549f7e91
Alexsander Alves <alvesalexsander@live.com>
2021-12-03 12:08:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+
+const mode = process.argv[2];
+const inputPath = process.argv[3];
+
+const watermark = `# GENERATED WITH generate-env-template.js\n\n`;
+let fileContent = '';
+
+console.log('generate-env-template :: STARTING');
+switch (mode) {
+    case '-f':
+        generateEnvTemplate(inputPath);
+        console.log('generate-env-template :: DONE');
+        break;
+    case '-r':
+        recursiveGenerateEnv(getPath(inputPath));
+        console.log('generate-env-template :: DONE');
+        break;
+    default:
+        console.log('generate-env-template :: ERROR :: operation not supported');
+}
+
+
+function recursiveGenerateEnv(path) {
+    if (!fs.lstatSync(path)?.isDirectory()) {
+        return;
+    }
+
+    if (hasDotEnvFile(path)) {
+        generateEnvTemplate(path + '/.env');
+    }
+
+    const innerDirs = getDirectories(getPath(path));
+    if (innerDirs.length) {
+        for (const dir of innerDirs) {
+            recursiveGenerateEnv(getPath(path + `/${dir}`));
+        }
+    }
+}
+
+function getPath(inputPath) {
+    try {
+        return path.resolve(inputPath);
+    } catch (err) {
+        return '';
+    }
+}
+
+function hasDotEnvFile(dirPath) {
+    return fs.existsSync(dirPath + '/.env');
+}
+
+function generateEnvTemplate(path) {
+    fileContent = fs.readFileSync(getPath(path)).toString().replace(/=.*/gm, '=');
+
+    if (fileContent) {
+        const lastDirectorySlash = path.lastIndexOf('/') ?? inputPath.lastIndexOf('\\');
+        fs.writeFileSync(`${path.substr(0, lastDirectorySlash + 1)}env.tmpl`, watermark + fileContent);
+        console.log(`GENERATING ENV TEMPLATE AT ${getPath(path.substr(0, lastDirectorySlash + 1))}/env.tmpl`);
+        fileContent = '';
+    }
+}
+
+function getDirectories(path) {
+    return fs.readdirSync(path, { withFileTypes: true })
+        .filter(dir => dir.isDirectory())
+        .map(dir => dir.name);
+}
